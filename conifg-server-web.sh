abd41c1180701d87172c0265965bfa5fed6d4e45
Julio Cézar <julio.ciec@gmail.com>
2020-07-01 10:02:42 -0400

    Create conifg-server-web.sh

---
+++ conifg-server-web.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+x="local"
+
+echo "============================================================="
+echo "= Configurando Servidor Linux Web - Apache2, PHP7.4 e MySql ="
+echo "============================================================="
+
+sudo apt update
